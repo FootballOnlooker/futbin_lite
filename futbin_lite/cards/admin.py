1c1bee55c3ce4bc517c244a6fe281871f39813bf
FootballOnlooker <andreastim2022@gmail.com>
2025-10-02 21:46:42 +0200

    Day 2: add PlayerCard model and admin customization

--- futbin_lite/cards/admin.py
+++ futbin_lite/cards/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Player
+from .models import Player, PlayerCard
 
 # Register your models here.
 
 admin.site.register(Player)
+
+
+@admin.register(PlayerCard)
+class PlayerCardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rating', 'position', 'club', 'nation')
--- futbin_lite/cards/models.py
+++ futbin_lite/cards/models.py
@@ -12,3 +12,22 @@ class Player(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.position})"
+
+
+class PlayerCard(models.Model):
+    name = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    position = models.CharField(max_length=10)
+    club = models.CharField(max_length=100)
+    nation = models.CharField(max_length=100)
+    pace = models.IntegerField()
+    shooting = models.IntegerField()
+    passing = models.IntegerField()
+    dribbling = models.IntegerField()
+    defending = models.IntegerField()
+    physical = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} ({self.rating})'
+
+

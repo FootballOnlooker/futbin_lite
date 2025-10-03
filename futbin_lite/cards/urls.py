7d5fe0530405399ebcf30dfc598207498936455f
FootballOnlooker <andreastim2022@gmail.com>
2025-10-03 11:41:38 +0200

    Day 3: add player list view and template players

---
+++ futbin_lite/cards/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("players/", views.player_list, name="player_list"),
+]
--- futbin_lite/cards/views.py
+++ futbin_lite/cards/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import PlayerCard
+
 
 # Create your views here.
+def player_list(request):
+    players = PlayerCard.objects.all()
+    return render(request, 'cards/player_list.html', {'players': players})
